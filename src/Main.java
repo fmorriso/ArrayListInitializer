5139c3fee2bb1010648b330e108eddf566309402
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-03-17 08:42:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(56, 32, 48));
+	    System.out.println(nums);
+    }
+}
